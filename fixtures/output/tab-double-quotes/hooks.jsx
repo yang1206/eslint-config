03eca0544fdb29006f74b234459cfdb8aa2da380
yang1206 <yang15188280673@outlook.com>
2023-12-23 15:59:39 +0800

    test: error

---
+++ fixtures/output/tab-double-quotes/hooks.jsx
@@ -0,0 +1,18 @@
+import { useEffect, useState } from "react"
+
+export function Hi() {
+	const [count, setCount] = useState(0)
+	if (true) {
+		useEffect(() => {
+			// eslint-disable-next-line no-console
+			console.log(count + 1)
+		}, [])
+	}
+
+	return (
+		<div>
+			<div>{count}</div>
+			<button onClick={setCount(c => c + 1)}>+1</button>
+		</div>
+	)
+}
--- test/fixtures.test.ts
+++ test/fixtures.test.ts
@@ -89,9 +89,9 @@ function runWithConfig(name: string, configs: OptionsConfig, ...items: FlatConfi
     })
     await fs.writeFile(join(target, 'eslint.config.js'), `
 // @eslint-disable
-import antfu from '@antfu/eslint-config'
+import eslint from '@yang1206/eslint-config'
 
-export default antfu(
+export default eslint(
   ${JSON.stringify(configs)},
   ...${JSON.stringify(items) ?? []},
 )
